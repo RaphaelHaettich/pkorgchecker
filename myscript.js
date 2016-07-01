5b4ac0e18ced20f8874387e97779fd88068ea07a
Philipp Marmet <mail@nytoca.ch>
2016-07-01 13:15:38 +0200

    fixed indentation

--- myscript.js
+++ myscript.js
@@ -1,13 +1,15 @@
 var detaildaten = document.documentElement.innerHTML.indexOf('Detaildaten');
+
 if(detaildaten != -1){
-var position = document.documentElement.innerHTML.indexOf('bestanden');
-var position1 = document.documentElement.innerHTML.indexOf('Bestanden');
-if(position != -1 || position1 != -1){
-alert("FOUND!");
-}else{
-  console.log("nothing yet")
-  setTimeout(function(){
-     window.location.reload(1);
-  }, 5000);
+  var position = document.documentElement.innerHTML.indexOf('bestanden');
+  var position1 = document.documentElement.innerHTML.indexOf('Bestanden');
+  
+  if(position != -1 || position1 != -1){
+    alert("FOUND!");
+  } else {
+    console.log("nothing yet")
+    setTimeout(function(){
+       window.location.reload(1);
+    }, 5000);
 }
 }
